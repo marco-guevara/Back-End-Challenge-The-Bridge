0f642a38e81b599cc7d619c1ae3ec0e60201e6bf
Marco Guevara <marcojgh352@gmail.com>
2026-05-25 13:50:17 +0200

    Revert "refactor: update User model to use UUIDs and improve field definitions"
    This reverts commit 06554886b303e09f36c29621fbbdc5544ef5b633.

---
+++ src/models/User.js
@@ -0,0 +1,43 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+
+const User = sequelize.define(
+    "User",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            field: "password_hash",
+        },
+        role: {
+          type: DataTypes.ENUM("junior", "senior"),
+          allowNull: false,
+          defaultValue: "junior"
+        }
+        
+    },
+    {
+        tableName: "users",
+        timestamps: true,
+        underscored: true,
+    }
+);
+
+module.exports = User;
